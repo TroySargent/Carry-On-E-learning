058b52cd8f108c7d4b99888f4914a2a6817d1529
Troy Sargent <troyjsargent@gmail.com>
2020-10-02 13:04:50 -0500

    change / route

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "development") {
 } else {
   
   // Handles any requests that don't match the ones above
-  app.get('*', (req,res) =>{
+  router.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname,'/client/build/index.html'));
   });
 
